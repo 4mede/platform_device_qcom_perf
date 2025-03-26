44a8e1a8cb056cb24b09fdf1594ae7199889fa6b
AmeChanRain <downloadbot007@gmail.com>
2025-03-26 17:37:20 +0800

    fixup! perf: Add vendor suffix for perf hals

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -31,6 +31,31 @@ function vendor_imports() {
 EOF
 }
 
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        vendor.qti.hardware.iop@2.0 | \
+            vendor.qti.hardware.perf@2.0 | \
+            vendor.qti.hardware.perf@2.1 | \
+            vendor.qti.hardware.perf@2.2 | \
+            vendor.qti.qspmhal@1.0)
+            echo "$1-vendor"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
